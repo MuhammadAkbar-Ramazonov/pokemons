10545297e72ffa434c613b3fd1568fb8e63ffbdf
John Doe <ramazonov3600@gmail.com>
2022-11-03 09:49:23 +0500

    finishshsh

--- mian.js
+++ mian.js
@@ -5,8 +5,6 @@ const newPokemonFregment = document.createDocumentFragment();
 const elForm = document.querySelector(".site-form");
 const elSelect = document.querySelector(".site-select");
 const elFormInput = document.querySelector(".js-input");
-const elSearchStart = document.querySelector(".form-search-start");
-const elSearchEnd = document.querySelector(".form-search-end");
 const elSelectSort = document.querySelector(".site-select-sort");
 const weaknesSum = [];
 
@@ -17,33 +15,29 @@ function sortFunc(arr, select){
       if(String(a.name) > String(b.name)){
         return 1;
       }
-      else if(String(a.name) < String(b.name)){
+      if(String(a.name) < String(b.name)){
         return -1;
       }
-      else{
-        return 0;
-      }
+      return 0;
     });
   }
   else if(select == "Z-A"){
     arr.sort((a, b) =>{
-      if(String(a.name) > String(b.name)){
+      if(a.name > b.name){
         return -1;
       }
-      else if(String(a.name) < String(b.name)){
+      if(a.name < b.name){
         return 1;
       }
-      else{
-        return 0;
-      }
+      return 0;
     })
   }
   
-  if(select == "2000-2018"){
+  if(select == "01:30-23:40"){
     arr.sort((a, b) => a.spawn_time.charCodeAt(0) - b.spawn_time.charCodeAt(0));
   }
-  else if(select == "2018-2000"){
-    arr.sort((a, b) => b.spawn_time.charCodeAt(0) - a.spawn_timecharCodeAt(0));
+  else if(select == "23:40-01:30"){
+    arr.sort((a, b) => b.spawn_time.charCodeAt(0) - a.spawn_time.charCodeAt(0));
   }
 };  
 
@@ -53,7 +47,7 @@ function createElement(kino) {
   kino.forEach(item => {
     
     const elClonePekemon = elTemplate.cloneNode(true);
-    item.weaknesses.forEach(function(element){
+    item.type.forEach(function(element){
       if(!weaknesSum.includes(element)){
         weaknesSum.push(element);
         const elOption = document.createElement("option");
@@ -80,33 +74,29 @@ elForm.addEventListener("submit", function(evt){
   const elSelctCatecoryValue = elSelect.value;
   const elFormInputValue = elFormInput.value.trim();
   const elSelectValue = elSelect.value.trim();
-  const elSearchStartValue = Number(elSearchStart.value);
-  const elSearchEndValue = Number(elSearchEnd.value);
   const regexValue = new RegExp(elFormInputValue, "gi");
+  const regexCategory = new RegExp(elSelctCatecoryValue, "gi");
   const elSelectSortValue = elSelectSort.value;
   
-  sortFunc(pokemons, elSelectSortValue);
   
   if(elFormInputValue == ""){
-    const searchMovie = pokemons.filter(item => ((elSearchStartValue <= item.spawn_time && elSearchEndValue >= item.spawn_time) || (elSearchStartValue == "" && elSearchEndValue >= item.spawn_time) || (elSearchStartValue <= item.spawn_time && elSearchEndValue == "")));
+    const searchMovie = pokemons.filter(item => (item.type.join(" ").match(regexCategory) || elSelctCatecoryValue === "All"));
+    sortFunc(pokemons, elSelectSortValue);
     createElement(searchMovie.slice(0));
   }
   else{
-    const searchMovie = pokemons.filter(item => String(item.name).match(regexValue) && (elSelectValue === item.weaknesses || String(item.name).match(regexValue) && elSelectValue === "All")  && ((elSearchStartValue <= item.spawn_time && elSearchEndValue >= item.spawn_time) || (elSearchStartValue == "" && elSearchEndValue >= item.spawn_time) || (elSearchStartValue <= item.spawn_time && elSearchEndValue == "")));
+    const searchMovie = pokemons.filter(item => String(item.name).match(regexValue) && (elSelectValue === item.type.join("") || elSelectValue === "All"));
+    
+    sortFunc(searchMovie, elSelectSortValue);
     
     createElement(searchMovie.slice(0));
-
+    
     if (searchMovie.length > 0){
       createElement(searchMovie);
     }else{
       elMoviesList.innerHTML = "Not found !!!";
     }
-  };
+  }  
 })
 
 createElement(pokemons.slice(0, 10));
-
-
-var benom = "dod";
-
-benom.charCodeAt()
